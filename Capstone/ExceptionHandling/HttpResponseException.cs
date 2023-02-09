96062d7dedeb001764c14da8125bc4012aa7eb61
Utsuhoagie <thiendangssb@gmail.com>
2023-02-09 21:24:32 +0700

    add: custom response exception

---
+++ Capstone/ExceptionHandling/HttpResponseException.cs
@@ -0,0 +1,13 @@
+﻿using System.Net;
+
+namespace Capstone.ExceptionHandling
+{
+	public class HttpResponseException : Exception
+	{
+		public int StatusCode { get; }
+		public object? Value { get; }
+
+		public HttpResponseException(HttpStatusCode statusCode, object? value = null) =>
+			(StatusCode, Value) = ((int) statusCode, value);
+	}
+}
